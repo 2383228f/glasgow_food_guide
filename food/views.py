d620bcc8b2b11e89acdb08eb8d51e3bb0f2529ab
2382727L <60507676+2382727L@users.noreply.github.com>
2020-04-02 17:08:14 +0100

    Formatting changes+more file handling
    Upload still doesn't quite work

--- food/views.py
+++ food/views.py
@@ -110,8 +110,7 @@ def add_restaurant(request):
             # Save the new category to the database.
             form = form.save(commit=False)
             form.owner = UserProfile.objects.get(username=request.user.username)
-            if form.picture != None:
-                form.model_pic = form.cleaned_data['picture']
+            print(form.picture)
             form.save()
             # Now that the category is saved, we could confirm this.
             # For now, just redirect the user back to the index view.
