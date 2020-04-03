c9cb0d5eb86b88abba27bdda51dda76a10fd66ad
Rhodri Rees <2373655r@student.gla.ac.uk>
2020-04-03 16:08:14 +0100

    added some model tests

--- food/tests.py
+++ food/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from food.models import Restaurant
+from food.models import Comment
+class Tests(TestCase):
+	#create restaurant
+	def test_create_rest(self):
+		rest = Restaurant(restaurant_ID = 14351)
+		rest.save()
+		self.assertEqual((rest.restaurant_ID), 14351)
+	#restaurant default location is glasgow
+	def test_create_rest(self):
+		rest = Restaurant(restaurant_ID = 14351)
+		rest.save()
+		self.assertEqual(((rest.lat,rest.lng)), (55.8642,-4.2518))
+	#create comment
+	def test_create_rest(self):
+		rest = Restaurant(restaurant_ID = 14214)
+		rest.save()
+		com = Comment(review_ID = 51453, rating =3, price =3,restaurant = rest)
+		com.save()
+		self.assertEqual((com.review_ID), 51453)
