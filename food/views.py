d115f3cdd73abfca77b491696db1b4d72db595de
2383228f <2383228f@student.gla.ac.uk>
2020-04-03 15:28:41 +0100

    Django error no longer occurs when creating a restaurant that no longer exists

--- food/views.py
+++ food/views.py
@@ -109,6 +109,12 @@ def add_restaurant(request):
     # A HTTP POST?
     if request.method == 'POST':
         form = RestaurantForm(request.POST, request.FILES)
+        form = form.save(commit=False)
+        if Restaurant.objects.get(slug=slugify(form.name)):
+            return redirect(reverse('food:show_restaurant',kwargs={'restaurant_name_slug':slugify(form.name)}))
+        
+        
+       
         # Have we been provided with a valid form?
         if form.is_valid():
             # Save the new category to the database.
